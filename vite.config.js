962583b56f39661e5616b3f62ac2334ef54f02ca
Sagar Saha <ss2749968@gmail.com>
2025-02-10 13:58:37 +0530

    changed vite config

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: "0.0.0.0", // This allows connections from any IP address
+    port: 5173, // Optional, in case you want to change the port
+  },
 });
